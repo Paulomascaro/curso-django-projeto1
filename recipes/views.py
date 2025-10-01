eb72163c6f7f337a62e5ff21129ab52e561bbb23
Paulo R. Mascaro <mascaropr@gmail.com>
2025-10-01 09:47:58 -0400

    templates e render

--- recipes/views.py
+++ recipes/views.py
@@ -5,7 +5,9 @@ from django.shortcuts import render
 
 # HTTP REQUEST
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'recipes/home.html', context={
+        'name': 'Paulo R. Mascaro',
+    })
 
 def contato(request):
     return HttpResponse('contato')
