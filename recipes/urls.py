091b517464a80a06dc4aca80fe82a1d4b0a89831
Paulo R. Mascaro <mascaropr@gmail.com>
2025-10-07 13:13:49 -0400

    View URLs Django

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,8 +1,10 @@
-from django.urls import path
+from django.urls import path, re_path
 from recipes.views import home
+from . import views
 
 
 urlpatterns = [ 
-    path('', home), # Home
+    path('', views.home), # Home
+    path('recipes/<int:id>/', views.recipe), # Recipe
    
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -7,3 +7,9 @@ def home(request):
     return render(request, 'recipes/pages/home.html', context={
         'name': 'Paulo R. Mascaro',
     })
+
+
+def recipe(request, id):
+    return render(request, 'recipes/pages/home.html', context={
+        'name': 'Paulo R. Mascaro',
+    })
