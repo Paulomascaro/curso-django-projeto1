da8f61d0bfef5dd47b1b767b7f870fdddcf21b83
Paulo R. Mascaro <mascaropr@gmail.com>
2026-09-06 22:23:20 -0400

    feat: Removido contato e sobre

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path
 
-from recipes.views import sobre, contato, home
+from recipes.views import home
 
 
 urlpatterns = [
-    path('', home),
-    path('sobre/', sobre),
-    path('contato/', contato),
+    path('', home),  # Home
 ]
