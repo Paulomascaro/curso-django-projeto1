d521eed0788ef76718af8d402d1cb613a13e33a9
Paulo R. Mascaro <mascaropr@gmail.com>
2026-09-12 13:45:25 -0400

    feat: funcionalidade: adiciona visualizações (views) de home e receitas e padrões de url

--- recipes/views.py
+++ recipes/views.py
@@ -5,3 +5,9 @@ def home(request):
     return render(request, 'recipes/pages/home.html', context={
         'name': 'Paulo Mascaro',
     })
+
+
+def recipe(request, id):
+    return render(request, 'recipes/pages/recipe.html', context={
+        'name': 'Paulo Mascaro',
+    })
