2aa6c099579a92c46e9a5c29579512b69de16e90
Paulo R. Mascaro <mascaropr@gmail.com>
2026-09-11 22:10:50 -0400

    feat: adiciona configurações iniciais do projeto com o aplicativo recipes e suas configurações

--- projeto/settings.py
+++ projeto/settings.py
@@ -130,6 +130,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'base_static',
 ]
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Email
 # https://docs.djangoproject.com/en/6.1/topics/email/#topic-email-configuration
