10de26337c670f7e73e5e60a1bb7b11c34fed8bf
Paulo R. Mascaro <mascaropr@gmail.com>
2026-09-16 14:33:53 -0400

    feat: Inicializanado projeto Django com modelos, admin e configuração do app de receitas.

--- recipes/admin.py
+++ recipes/admin.py
@@ -5,4 +5,10 @@ from .models import Category, Recipe
 class CategoryAdmin(admin.ModelAdmin):
     pass
 
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    pass
+
+
 admin.site.register(Category, CategoryAdmin)
